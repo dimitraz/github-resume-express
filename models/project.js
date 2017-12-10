24b2579d043294fdea1ab26693821a468917f29d
dimitraz <dimitrazuccarelli@gmail.com>
2017-12-10 15:52:43 +0000

    Add schema validation

--- models/project.js
+++ models/project.js
@@ -1,16 +1,28 @@
-
 var mongoose = require('mongoose');
+var validators = require('mongoose-validators');
 var Schema = mongoose.Schema;
 
 var ProjectSchema = new Schema({
-    name: { type: String, required: true },
-    userId: { type: String, required: true },
+    name: { type: String, required: [true, 'Project name is required'] },
+    userId: { type: String, required: [true, 'User Id is required'] },
     description: String,
-    language: String,
-    html_url: String,
-    ext_url: String,
+    language: {
+        type: String,
+        validate: validators.isAlphanumeric()
+    },
+    html_url: {
+        type: String,
+        validate: validators.isURL({ message: 'Please enter a valid URL'})
+    },
+    ext_url: {
+        type: String,
+        validate: validators.isURL({ message: 'Please enter a valid URL'})
+    },
     company: String, 
-    avatar_url: String
+    avatar_url: {
+        type: String,
+        validate: validators.isURL({ message: 'Please enter a valid avatar URL'})
+    }
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
--- models/user.js
+++ models/user.js
@@ -1,18 +1,25 @@
 var mongoose = require('mongoose');
+var validators = require('mongoose-validators');
 var Schema = mongoose.Schema;
 
 var InterestSchema = new Schema({
-    interest: { type: String, required: true } 
+    interest: { type: String, required: [true, 'Please enter a valid interest'] }
 });
 
 var UserSchema = new Schema({
-    login: { type: String, required: true },
-    name: { type: String, required: true },
+    login: { type: String, required: [true, 'User login name is required'] },
+    name: { type: String, required: [true, 'Name is required'] },
     bio: String,
     location: String,
-    blog: String,
-    email: String,
-    company: String, 
+    blog: {
+        type: String,
+        validate: validators.isURL({ message: 'Please enter a valid blog URL'})
+    },
+    email: {
+        type: String,
+        validate: validators.isEmail({ message: 'Please enter a valid email address'})
+    },
+    company: String,
     avatar_url: String,
     interests: [InterestSchema],
 });
