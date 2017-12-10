028677674000a13fd94b2db692c712ae32b3c0bd
dimitraz <dimitrazuccarelli@gmail.com>
2017-12-10 17:22:10 +0000

    More user schema validation

--- models/user.js
+++ models/user.js
@@ -2,25 +2,58 @@ var mongoose = require('mongoose');
 var validators = require('mongoose-validators');
 var Schema = mongoose.Schema;
 
+function deleteEmpty(v) {
+    if (v == null | v === "") {
+        return undefined;
+    }
+    return v;
+}
+
 var InterestSchema = new Schema({
     interest: { type: String, required: [true, 'Please enter a valid interest'] }
 });
 
 var UserSchema = new Schema({
-    login: { type: String, required: [true, 'User login name is required'] },
-    name: { type: String, required: [true, 'Name is required'] },
-    bio: String,
-    location: String,
+    login: { 
+        type: String, 
+        unique: [true, 'A user with that login already exists'],
+        required: [true, 'User login name is required'] 
+    },
+    name: { 
+        type: String, 
+        required: [true, 'Name is required'] 
+    },
+    github_id: { 
+        type: Number, 
+        unique: [true, 'A user with that id already exists'],
+        required: [true, 'Github id is required']
+    },
+    bio: {
+        type: String,
+        set: deleteEmpty
+    },
+    location: {
+        type: String,
+        set: deleteEmpty
+    },
     blog: {
         type: String,
-        validate: validators.isURL({ message: 'Please enter a valid blog URL'})
+        set: deleteEmpty,
+        validate: validators.isURL({ message: 'Please enter a valid blog URL' })
     },
     email: {
         type: String,
-        validate: validators.isEmail({ message: 'Please enter a valid email address'})
+        set: deleteEmpty,
+        validate: validators.isEmail({ message: 'Please enter a valid email address' })
     },
-    company: String,
-    avatar_url: String,
+    company: {
+        type: String,
+        set: deleteEmpty
+    },
+    avatar_url: {
+        type: String,
+        set: deleteEmpty
+    },
     interests: [InterestSchema],
 });
 
